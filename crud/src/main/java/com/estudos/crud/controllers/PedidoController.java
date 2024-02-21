e2a8fffb7bc70586298b034b2f71f948a5cfcc2a
Michel Machado <michelmachado125@gmail.com>
2024-02-21 18:26:20 -0300

    CROSS ORIGIN"*"

--- crud/src/main/java/com/estudos/crud/controllers/PedidoController.java
+++ crud/src/main/java/com/estudos/crud/controllers/PedidoController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "crud-repository-production.up.railway.app")
+@CrossOrigin("*")
 @RequestMapping("/pedido")
 public class PedidoController {
 
--- crud/src/main/java/com/estudos/crud/controllers/ProdutoController.java
+++ crud/src/main/java/com/estudos/crud/controllers/ProdutoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "crud-repository-production.up.railway.app")
+@CrossOrigin("*")
 @RequestMapping("/produto")
 public class ProdutoController {
 
