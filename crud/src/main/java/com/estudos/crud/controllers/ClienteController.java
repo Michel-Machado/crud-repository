4bdcde26308998820c05f4c52eb15d7c6dc1fa7f
Michel Machado <michelmachado125@gmail.com>
2024-02-21 19:05:05 -0300

    CROSS ORIGIN"*"

--- crud/src/main/java/com/estudos/crud/controllers/ClienteController.java
+++ crud/src/main/java/com/estudos/crud/controllers/ClienteController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "crud-repository-production.up.railway.app")
+@CrossOrigin(origins = "*")
 @RequestMapping("/cliente")
 public class ClienteController {
 
