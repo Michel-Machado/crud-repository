0d4c40f9fb591d919cca3750a9e3eaeb26f2eed8
Michel Machado <michelmachado125@gmail.com>
2024-02-21 16:26:33 -0300

    configuração docker

--- crud/src/main/java/com/estudos/crud/CrudApplication.java
+++ crud/src/main/java/com/estudos/crud/CrudApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudApplication {
+	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
